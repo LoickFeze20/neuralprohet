218c7ba2da39fdee047ac148beb16dcbf29ff2a7
Loick_Feze <fezejordan@gmail.com>
2026-02-19 12:12:32 +0100

    Update neural.py

--- neural.py
+++ neural.py
@@ -8,7 +8,15 @@ from neuralprophet import NeuralProphet
 from datetime import datetime, timedelta
 import torch
 import os
+import sys
 
+# Forcer l'import de pkg_resources si nécessaire
+try:
+    import pkg_resources
+except ImportError:
+    import subprocess
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "setuptools"])
+    import pkg_resources
 # Configuration de la page
 st.set_page_config(
     page_title="Apple NeuralProphet Predictor",
@@ -561,6 +569,7 @@ st.markdown("""
 </div>
 
 """, unsafe_allow_html=True)
+
 
 
 
